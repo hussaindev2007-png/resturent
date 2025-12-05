3e39ec5589ea3d63c65c095be300f324b9510160
Hussain Ali <hussaindev2007@gmail.com>
2025-12-05 20:07:02 +0500

    Update Products.jsx

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,75 +1,4 @@
 
-
-
-
-// import ProductCard from "./ProductCard";
-
-// function Products({ products = [], selectedRating, search }) {
-  
-
-//   const searchedProducts = products.filter((item) =>
-//     item.title.toLowerCase().includes(search.toLowerCase())
-//   );
-
-
-//   if (products.length === 0) {
-//     return (
-//       <div className="text-center text-gray-500 text-lg py-10">
-//         No matching products
-//       </div>
-//     );
-//   }
-
-
-//   if (searchedProducts.length === 0 && search.trim() !== "") {
-//     return (
-//       <p
-//         style={{
-//           color: "#04d7f3ff",
-//           marginTop: "15px",
-//           fontSize: "18px",
-//           textAlign: "center",
-//         }}
-//       >
-//         No products found for "<strong>{search}</strong>"
-//       </p>
-//     );
-//   }
-
-//   return (
-//     <div className="grid grid-cols-3 gap-6 p-6">
-//       {searchedProducts.map((product) => (
-//         <ProductCard
-//           key={product.id}
-//           product={product}
-//           selectedRating={selectedRating}
-
-          
-//         />
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default Products;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
 
@@ -77,7 +6,6 @@ function Products({ selectedRating, search }) {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  // Fetch products from JSON Server
   useEffect(() => {
     fetch("http://localhost:3001/products")
       .then((res) => {
@@ -94,7 +22,8 @@ function Products({ selectedRating, search }) {
       });
   }, []);
 
-  // Filter products based on search
+
+  
   const searchedProducts = products.filter((item) =>
     item.title.toLowerCase().includes(search.toLowerCase())
   );
@@ -138,3 +67,4 @@ function Products({ selectedRating, search }) {
 }
 
 export default Products;
+
